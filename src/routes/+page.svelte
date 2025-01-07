b2897b8abc09df21ad3a0992707e9013674e6c99
Zach Pieratt <zachpieratt@gmail.com>
2025-01-06 21:21:50 -0500

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import he from 'he';
+	import { onMount } from 'svelte';
+
+	const endpoint = 'https://opentdb.com/api.php?amount=1';
+	let questions = [];
+
+	onMount(async function () {
+		const response = await fetch(endpoint, { method: 'GET', mode: 'cors' });
+		const data = await response.json();
+		questions = data.results;
+		console.log(questions);
+	});
+</script>
+
+<div class="header">
+	<h1>Trivia</h1>
+	<a href="/about">About my site</a>
+</div>
+<div class="body">
+	{#each questions as question}
+		<div>{he.decode(question.question)}</div>
+	{/each}
+</div>
+
+<style>
+	.header {
+		text-align: center;
+	}
+</style>
