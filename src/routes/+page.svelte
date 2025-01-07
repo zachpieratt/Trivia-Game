bdf8e5bd342ee8d9ca533e3f902521d94d72d39c
Zach Pieratt <zachpieratt@gmail.com>
2025-01-06 22:24:56 -0500

    added framework for questions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,14 +2,22 @@
 	import he from 'he';
 	import { onMount } from 'svelte';
 
-	const endpoint = 'https://opentdb.com/api.php?amount=1';
-	let questions = [];
+	const endpoint = 'https://opentdb.com/api.php?amount=10';
+	let questionData = [];
+
+	const questionTypes = {
+		boolean: 'True / False',
+		multiple: 'Multiple Choice'
+	};
 
 	onMount(async function () {
 		const response = await fetch(endpoint, { method: 'GET', mode: 'cors' });
 		const data = await response.json();
-		questions = data.results;
-		console.log(questions);
+		questionData = data.results;
+		for (let i = 0; i < questionData.length; i++) {
+			let answerArray = questionData[i].incorrect_answers;
+			answerArray.push(questionData[i].correct_answer);
+		}
 	});
 </script>
 
@@ -17,14 +25,35 @@
 	<h1>Trivia</h1>
 	<a href="/about">About my site</a>
 </div>
+<br />
 <div class="body">
-	{#each questions as question}
-		<div>{he.decode(question.question)}</div>
+	{#each questionData as question}
+		<div class="questionBox">
+			<p class="category">Category : {he.decode(question.category)}</p>
+			<p class="difficulty">Difficulty: {question.difficulty.toUpperCase()}</p>
+			<div class="questionType">Question Type: {questionTypes[question.type]}</div>
+			<br />
+			<div class="questionText">{he.decode(question.question)}</div>
+			{#if question.type == 'boolean'}
+				<button class="trueButton">True</button>
+				<button class="falseButton">False</button>
+			{/if}
+		</div>
 	{/each}
 </div>
 
 <style>
-	.header {
+	.header,
+	.difficulty,
+	.questionType {
 		text-align: center;
+	}
+	.category {
+		text-decoration: underline;
+		font-weight: bolder;
+		text-align: center;
+	}
+	.questionBox {
+		border: 1px solid black;
 	}
 </style>
